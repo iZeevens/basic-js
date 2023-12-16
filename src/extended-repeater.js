9ae60fa31e40c2e732ffe9e24767c44b691744a9
iZeevens <129598659+iZeevens@users.noreply.github.com>
2023-12-16 19:00:53 +0600

    fix: some problem

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,51 +1,54 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
 function repeater(str, options) {
-  let strOutput = '',
-        repeatTimes = options.repeatTimes ?? 1,
-        separator = options.separator ?? '+',
-        addition = options.addition ?? '',
-        additionRepeatTimes = options.additionRepeatTimes ?? '',
-        additionSeparator = options.additionSeparator ?? '';
+  let strOutput = "",
+    repeatTimes = options.repeatTimes ?? 1,
+    separator = options.separator ?? "+",
+    addition = options.addition ?? "",
+    additionRepeatTimes = options.additionRepeatTimes ?? 1,
+    additionSeparator = options.additionSeparator ?? "";
 
+  for (let repeatStr = 0; repeatStr < repeatTimes; repeatStr++) {
+    let repeatSeporator = repeatStr + 1;
+    strOutput += str;
 
-    for (let repeatStr = 0; repeatStr < repeatTimes; repeatStr++) {
-        let repeatSeporator = repeatStr + 1;
-        strOutput += str
-
-        for (let addStr = 0; addStr < additionRepeatTimes; addStr++) {
-            if (additionRepeatTimes) {
-                strOutput += addition;
-                if (additionSeparator) {
-                    strOutput += additionSeparator;
-                }
-            } else {
-                strOutput += addition;
-            }
+    for (let addStr = 0; addStr < additionRepeatTimes; addStr++) {
+      if (additionRepeatTimes) {
+        strOutput += addition;
+        if (
+          additionSeparator &&
+          additionRepeatTimes > 1 &&
+          additionRepeatTimes - addStr > 1
+        ) {
+          strOutput += additionSeparator;
         }
-
-       if (repeatSeporator < repeatTimes) {
-            strOutput += separator;
-       }
+      } else if (!additionRepeatTimes && additionRepeatTimes - addStr > 1) {
+        strOutput += addition;
+      }
     }
-    return strOutput;
+
+    if (repeatSeporator < repeatTimes) {
+      strOutput += separator;
+    }
+  }
+  return strOutput;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
