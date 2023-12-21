e421b7166590acb597e1c89c885445f436386ab9
iZeevens <129598659+iZeevens@users.noreply.github.com>
2023-12-22 00:44:45 +0600

    feat: add decrypt and some changes encrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -38,57 +38,90 @@ class VigenereCipheringMachine {
     return this.matrix;
   }
 
-  encrypt(world, key) {
-    const newWorld = world.toUpperCase().split("");
+  encrypt(word, key) {
+    if (!word || !key) {
+      throw Error("Incorrect arguments!");
+    }
+    let newWorld = word.toUpperCase().split("");
     let newKey = [],
       resultEncrypt = [];
 
     while (newKey.length < newWorld.length) {
       newKey.push(...key.toUpperCase().split(""));
     }
-
-    let indexArray = null,
-      indexWorld = null;
-
     this.matrixVigenere();
+    while (resultEncrypt.length !== word.length) {
+      let newWorldCurrentArray = newWorld[0],
+        indexArray = null,
+        indexWorld = null;
 
-    while (resultEncrypt.length !== world.length) {
-      if (newWorld[0] === " " || newWorld[0] === "!") {
+      if (newWorldCurrentArray === " " || newWorldCurrentArray === "!") {
         resultEncrypt.push(newWorld.shift());
         continue;
       }
 
-      if (indexWorld === null) {
-        for (let i = 0; i < this.matrix[0].length; i++) {
-          if (this.matrix[0][i] === newWorld[0]) {
-            indexWorld = i;
-            newWorld.shift();
-            break;
-          }
-        }
-      }
-      if (indexArray === null) {
-        for (let i = 0; i < this.matrix.length; i++) {
-          if (this.matrix[i][0] === newKey[0]) {
-            indexArray = i;
-            newKey.shift();
-            break;
-          }
+      for (let i = 0; i < this.matrix[0].length; i++) {
+        if (this.matrix[0][i] === newWorld[0]) {
+          indexWorld = i;
+          newWorld.shift();
+          break;
         }
       }
 
-      if (indexArray !== null && indexWorld !== null) {
-        resultEncrypt.push(this.matrix[indexArray][indexWorld]);
-        indexArray = null;
-        indexWorld = null;
+      for (let i = 0; i < this.matrix.length; i++) {
+        if (this.matrix[i][0] === newKey[0]) {
+          indexArray = i;
+          newKey.shift();
+          break;
+        }
       }
+
+      resultEncrypt.push(this.matrix[indexArray][indexWorld]);
     }
 
     return resultEncrypt.join("");
   }
-  decrypt() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  decrypt(encryptWord, key) {
+    if (!encryptWord || !key) {
+      throw Error("Incorrect arguments!");
+    }
+    const newEncryptWorld = encryptWord.toUpperCase().split("");
+    let newKeyWorld = [],
+      resultDecrypt = [];
+
+    while (newKeyWorld.length < newEncryptWorld.length) {
+      newKeyWorld.push(...key.toUpperCase().split(""));
+    }
+
+    this.matrixVigenere();
+    while (resultDecrypt.length !== encryptWord.length) {
+      let indexWord = null;
+
+      if (newEncryptWorld[0] === " " || newEncryptWorld[0] === "!") {
+        resultDecrypt.push(newEncryptWorld.shift());
+        continue;
+      }
+
+      for (let i = 0; i < this.matrix.length; i++) {
+        if (indexWord !== null) {
+          break;
+        }
+        if (this.matrix[i][0] === newKeyWorld[0]) {
+          newKeyWorld.shift();
+          for (let j = 0; j < this.matrix[i].length; j++) {
+            if (this.matrix[i][j] === newEncryptWorld[0]) {
+              indexWord = j;
+              newEncryptWorld.shift();
+              break;
+            }
+          }
+        }
+      }
+
+      resultDecrypt.push(this.matrix[0][indexWord]);
+    }
+
+    return resultDecrypt.join("");
   }
 }
 
