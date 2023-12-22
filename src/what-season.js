c3a9c156d89e9dda101339f6b6eae070b540e2b3
iZeevens <129598659+iZeevens@users.noreply.github.com>
2023-12-22 18:37:28 +0600

    refactor: del useless function and variables

--- src/what-season.js
+++ src/what-season.js
@@ -11,24 +11,12 @@ const { NotImplementedError } = require("../extensions/index.js");
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-
-function leapYear(year) {
-  if (year % 100 === 0 && year % 400 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 function getSeason(date) {
   if (!date) {
     return ("Unable to determine the time of year!");
   }
   try {
-    const year = date.getUTCFullYear(),
-    month = date.getUTCMonth() + 1,
-    day = date.getUTCDate();
-
+    const month = date.getUTCMonth() + 1
     
   if (month >= 3 && month <= 5) {
     return "spring";
